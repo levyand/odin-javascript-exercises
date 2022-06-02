ebe2ec9f7d585842a3f958dcf1dc11a9297662f6
Andrew Levy <levyand@gmail.com>
2022-06-02 10:26:47 -0500

    Complete Exercise: complete exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+  const args = [...arguments].slice(1);
+  return array.filter(val => !args.includes(val));
 };
 
 // Do not edit below this line
