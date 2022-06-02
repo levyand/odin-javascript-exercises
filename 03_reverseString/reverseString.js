f81a50d289036cdf98ea85cdf00f2bb88062e1b2
Andrew Levy <levyand@gmail.com>
2022-06-02 10:03:15 -0500

    Complete Exercise: complete exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  const chars = input.split('');
+  const reversedChars = chars.reverse();
+  return reversedChars.reduce((prev, curr) => prev + curr, '');
 };
 
 // Do not edit below this line
