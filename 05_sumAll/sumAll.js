6a4f821f7b0392847e6a8aef64671acbaeb5e2a1
Andrew Levy <levyand@gmail.com>
2022-06-02 11:02:48 -0500

    Complete Exercise: complete exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+  if (!Number.isInteger(int1) || !Number.isInteger(int2) ||
+      int1 < 0 || int2 < 0) {
+    return 'ERROR';
+  }
 
+  const start = int1 > int2 ? int2 : int1;
+  const length = Math.abs(int1 - int2) + 1;
+  const range = [...Array(length).keys()].map(val => val + start);
+  return range.reduce((prev, curr) => prev + curr);
 };
 
 // Do not edit below this line
